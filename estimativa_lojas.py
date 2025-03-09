dc79705f1cc88c02c8a4b2c8b1dea5078aa3a115
Dalton Farias <106979315+daltonffarias@users.noreply.github.com>
2025-03-09 11:32:41 -0300

    estimativa_lojas.py

--- estimativa_lojas.py
+++ estimativa_lojas.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 # Função para carregar os dados do GitHub
 def load_data():
-    url = "https://github.com/daltonffarias/lojas_atrasos/blob/main/inadimplencia_lojas.csv"
+    url = "https://raw.githubusercontent.com/daltonffarias/lojas_atrasos/main/inadimplencia_lojas.csv"
     data = pd.read_csv(url)
     return data
 
